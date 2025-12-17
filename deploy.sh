1e300681f458f185e69666745abc2d22bd497d51
root <root@ip-172-31-17-141.ap-south-1.compute.internal>
2025-12-17 00:27:00 +0000

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 docker stop devopsapp 2>/dev/null || true
 docker rm devopsapp 2>/dev/null || true
 
-docker pull write4ashok/Devops_pipeline:latest
+docker pull dockerfile:latest
 
 docker run -d \
   --name devopsapp \
   -p 80:5000 \
-  ashokdev/devops-project:latest
+  dockerfile:latest
 
